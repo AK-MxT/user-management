5fca0313eaadc8d42a7ddd106fc2514ca7aacbe4
MxT <ak4.greenworks@gmail.com>
2021-05-23 01:23:30 +0900

    ✨ライブラリ追加・Utilクラス作成・詳細画面更新

--- src/main/java/com/user/mng/domain/service/UserServiceImpl.java
+++ src/main/java/com/user/mng/domain/service/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.user.mng.domain.model.entity.UserListEntity;
 import com.user.mng.domain.model.response.UserDetailResponseEntity;
 import com.user.mng.domain.model.response.UserListResponseEntity;
 import com.user.mng.domain.repository.TrnUserMapper;
+import com.user.mng.utils.CommonUtils;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -46,10 +47,9 @@ public class UserServiceImpl implements UserService {
 			user.setFirstName(p.getFirstName());
 			user.setGender(UserConstant.GENDER_MAN.equals(p.getGender()) ? UserConstant.GENDER_MAN_VIEW
 					: UserConstant.GENDER_WOMAN_VIEW);
-			// TODO 日付のフォーマッタ作る
-			user.setBirthday(p.getBirthday().toString());
+			user.setBirthday(CommonUtils.formatToDate(p.getBirthday()));
 			user.setUpdateUser(p.getUpdateUser());
-			user.setUpdateDate(p.getUpdateDate().toString());
+			user.setUpdateDate(CommonUtils.formatToDateTime(p.getUpdateDate()));
 			user.setDeleteFlg(p.getDeleteFlg() ? UserConstant.DELETE_FLG_TRUE : UserConstant.DELETE_FLG_FALSE);
 
 			res.getUserList().add(user);
@@ -80,16 +80,16 @@ public class UserServiceImpl implements UserService {
 		detail.setFirstNameKana(result.getFirstNameKana());
 		detail.setGender(UserConstant.GENDER_MAN.equals(result.getGender()) ? UserConstant.GENDER_MAN_VIEW
 				: UserConstant.GENDER_WOMAN_VIEW);
-		// TODO 日付のフォーマッタ作る
-		detail.setBirthday(result.getBirthday().toString());
+		detail.setBirthday(CommonUtils.formatToDate(result.getBirthday()));
 		detail.setPostalCode(result.getPostalCode());
-		detail.setAddress(result.getPrefecture());
+		detail.setAddress(CommonUtils.formatAddress(result.getPrefecture(), result.getAddress1(), result.getAddress2(),
+				result.getAddress3(), result.getAddress4()));
 		detail.setPhoneNumber(result.getPhoneNumber());
 		detail.setRemarks(result.getRemarks());
 		detail.setInsertUser(result.getInsertUser());
-		detail.setInsertDate(result.getInsertDate().toString());
+		detail.setInsertDate(CommonUtils.formatToDateTime(result.getInsertDate()));
 		detail.setUpdateUser(result.getUpdateUser());
-		detail.setUpdateDate(result.getUpdateDate().toString());
+		detail.setUpdateDate(CommonUtils.formatToDateTime(result.getUpdateDate()));
 		detail.setDeleteFlg(result.getDeleteFlg() ? UserConstant.DELETE_FLG_TRUE : UserConstant.DELETE_FLG_FALSE);
 
 		res.setUser(detail);
---
+++ src/main/java/com/user/mng/utils/CommonUtils.java
@@ -0,0 +1,51 @@
+package com.user.mng.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * フォーマットとかデータ加工系
+ * 増えてきたらクラス分ける
+ */
+public final class CommonUtils {
+
+	/**
+	 * Date型の日付をフォーマットする
+	 *
+	 * @param date
+	 * @return フォーマット後の日付文字列（yyyy/MM/dd HH:mm:ss）
+	 */
+	public static String formatToDateTime (Date date) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String result = df.format(date);
+
+		return result;
+	}
+
+	/**
+	 * Date型の日付をフォーマットする
+	 *
+	 * @param date
+	 * @return フォーマット後の日付文字列（yyyy/MM/dd）
+	 */
+	public static String formatToDate (Date date) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+		String result = df.format(date);
+
+		return result;
+	}
+
+	public static String formatAddress (String prefecture, String add1, String add2, String add3, String add4) {
+
+		if (StringUtils.isEmpty(add3)) {
+			// 住所3が未入力の場合は住所2まで結合して返却
+			return prefecture + " " + add1 + " " + add2;
+		}
+
+		String address4 = StringUtils.isEmpty(add4) ? "" : " " + add4;
+
+		return prefecture + " " + add1 + " " + add2 + " " + add3 + address4;
+	}
+}
