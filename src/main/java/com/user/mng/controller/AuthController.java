3385c8695735eb3184f216b1b5d6e39fb3b11d6d
MxT <ak4.greenworks@gmail.com>
2021-06-15 03:41:24 +0900

    🔧Spring Security設定作成中（アカウント登録・PWエンコード）

--- src/main/java/com/user/mng/controller/AuthController.java
+++ src/main/java/com/user/mng/controller/AuthController.java
@@ -1,10 +1,21 @@
 package com.user.mng.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.user.mng.domain.service.LoginServiceImpl;
+
 @Controller
+@RequestMapping(value = "/login")
 public class AuthController {
+
+	@Autowired
+	private LoginServiceImpl loginServiceImpl;
+
+	@Autowired
+	private PasswordEncoder passwordEncorder;
 
 	/**
 	 * ログイン画面
@@ -12,8 +23,23 @@ public class AuthController {
 	 * @param model
 	 * @return ログイン画面
 	 */
-	@RequestMapping(value = "/login")
+	@RequestMapping(value = "/")
 	public String login() {
+		return "login";
+	}
+
+	/**
+	 * アカウント登録
+	 *
+	 * @param AccountRegisterRequestEntity アカウント登録用のリクエストエンティティ
+	 * @return ログイン画面
+	 */
+	@RequestMapping(value = "/register")
+	public String register(String userName, String password) {
+
+		// TODO ↑リクエストエンティティ作る・バリデーションも作る
+		loginServiceImpl.registerAccount(userName, passwordEncorder.encode(password));
+
 		return "login";
 	}
 }
--- src/main/java/com/user/mng/domain/service/LoginServiceImpl.java
+++ src/main/java/com/user/mng/domain/service/LoginServiceImpl.java
@@ -2,6 +2,7 @@ package com.user.mng.domain.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.util.StringUtils;
 
 import com.user.mng.constant.AuthConstant;
+import com.user.mng.constant.UserConstant;
 import com.user.mng.domain.model.TrnAccount;
 import com.user.mng.domain.model.TrnAccountExample;
 import com.user.mng.domain.model.entity.AccountEntity;
@@ -28,15 +30,15 @@ public class LoginServiceImpl implements UserDetailsService {
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
-		if (StringUtils.isEmpty(username)) {
+		if (StringUtils.isEmpty(userName)) {
 			throw new UsernameNotFoundException("username is empty.");
 		}
 
 		// 検索条件
 		TrnAccountExample filter = new TrnAccountExample();
-		filter.createCriteria().andUserNameEqualTo(username).andDeleteFlgEqualTo(Boolean.FALSE);
+		filter.createCriteria().andUserNameEqualTo(userName).andDeleteFlgEqualTo(Boolean.FALSE);
 
 		List<TrnAccount> accountList = trnAccountMapper.selectByExample(filter);
 
@@ -54,5 +56,36 @@ public class LoginServiceImpl implements UserDetailsService {
 				new SimpleGrantedAuthority(account.getAdminFlg() ? AuthConstant.ROLE_ADMIN : AuthConstant.ROLE_USER));
 
 		return new AccountEntity(account);
+	}
+
+
+	public void registerAccount(String userName, String password) {
+
+		// 検索条件
+		TrnAccountExample filter = new TrnAccountExample();
+		filter.createCriteria().andUserNameEqualTo(userName).andDeleteFlgEqualTo(Boolean.FALSE);
+
+		List<TrnAccount> accountList = trnAccountMapper.selectByExample(filter);
+
+		if (accountList.size() != 0) {
+			// 取得結果が0件でない場合、既に使われているユーザ名のため、エラー
+			// TODO 適切な例外クラスを指定する
+			throw new UsernameNotFoundException("This userName is already exists.");
+		}
+
+		// システム日時
+		Date now = new Date();
+
+		TrnAccount record = new TrnAccount();
+
+		record.setUserName(userName);
+		record.setPassword(password);
+		record.setAdminFlg(Boolean.FALSE);
+		record.setInsertDate(now);
+		record.setInsertUser(UserConstant.DEFAULT_USERNAME);
+		record.setUpdateDate(now);
+		record.setUpdateUser(UserConstant.DEFAULT_USERNAME);
+
+		trnAccountMapper.insertSelective(record);
 	}
 }
