62ddebb514cd980debfa59f010d023d4a299fd84
MxT <ak4.greenworks@gmail.com>
2021-07-04 02:04:30 +0900

    🐛MockmvcがNULLとなる問題を解消（AutoConfigureMockMvcアノテーションの追加）

--- src/test/java/com/user/mng/controller/UserControllerTest.java
+++ src/test/java/com/user/mng/controller/UserControllerTest.java
@@ -5,18 +5,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import com.user.mng.domain.service.UserService;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
+	@Autowired
 	private MockMvc mockmvc;
+
+	@MockBean
+	UserService mockUserService;
 
 	@Autowired
 	UserController target;
@@ -27,8 +36,8 @@ class UserControllerTest {
 	}
 
 	@Test
-	void isStatusTest() throws Exception {
-		mockmvc.perform(get("/user/list/1")).andExpect(status().isOk());
+	void getListTest() throws Exception {
+		mockmvc.perform(get("/user/list/1")).andExpect(status().is3xxRedirection());
+		//		.andExpect(view().name("login"));
 	}
-
 }
