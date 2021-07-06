73e40a0e64349be31d7861ed9c83225255672fae
MxT <ak4.greenworks@gmail.com>
2021-07-07 02:50:18 +0900

    🚧SpringSecurity用のテストクラス実装中（DBアクセスしないので、モックデータを用意する必要がありそう）

--- src/test/java/com/user/mng/controller/UserControllerTest.java
+++ src/test/java/com/user/mng/controller/UserControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -36,8 +38,16 @@ class UserControllerTest {
 	}
 
 	@Test
-	void getListTest() throws Exception {
+	@WithAnonymousUser
+	void ログインせずにアクセスするとリダイレクトされる() throws Exception {
 		mockmvc.perform(get("/user/list/1")).andExpect(status().is3xxRedirection());
+		//		.andExpect(view().name("login"));
+	}
+
+	@Test
+	@WithUserDetails("user001")
+	void ログイン後にアクセスすると正常終了() throws Exception {
+		mockmvc.perform(get("/user/list/1")).andExpect(status().isOk());
 		//		.andExpect(view().name("login"));
 	}
 }
