fff5342fda4a8aab06c856802d8b815bff57e650
MxT <ak4.greenworks@gmail.com>
2021-07-08 02:21:57 +0900

    🔧DbUnitの追加・テスト用CSVデータのロード処理を実装中

--- src/test/java/com/user/mng/controller/UserControllerTest.java
+++ src/test/java/com/user/mng/controller/UserControllerTest.java
@@ -1,28 +1,27 @@
 package com.user.mng.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.user.mng.domain.service.UserService;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
 
+	// mockMvc TomcatサーバへデプロイすることなくHTTPリクエスト・レスポンスを扱うためのMockオブジェクト
 	@Autowired
 	private MockMvc mockmvc;
 
@@ -40,14 +39,14 @@ class UserControllerTest {
 	@Test
 	@WithAnonymousUser
 	void ログインせずにアクセスするとリダイレクトされる() throws Exception {
-		mockmvc.perform(get("/user/list/1")).andExpect(status().is3xxRedirection());
+		this.mockmvc.perform(get("/user/list/1")).andExpect(status().is3xxRedirection());
 		//		.andExpect(view().name("login"));
 	}
 
 	@Test
 	@WithUserDetails("user001")
 	void ログイン後にアクセスすると正常終了() throws Exception {
-		mockmvc.perform(get("/user/list/1")).andExpect(status().isOk());
+		this.mockmvc.perform(get("/user/list/1")).andDo(print()).andExpect(status().isOk());
 		//		.andExpect(view().name("login"));
 	}
 }
