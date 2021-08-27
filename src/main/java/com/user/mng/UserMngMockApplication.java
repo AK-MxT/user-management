96aabe833b15c0278e4ecec42a483d8f4dc68a33
MxT <ak4.greenworks@gmail.com>
2021-08-28 02:55:32 +0900

    🔧warでデプロイするための設定を追記

--- src/main/java/com/user/mng/UserMngMockApplication.java
+++ src/main/java/com/user/mng/UserMngMockApplication.java
@@ -3,6 +3,8 @@ package com.user.mng;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Jsr330ScopeMetadataResolver;
 
@@ -11,7 +13,13 @@ import org.springframework.context.annotation.Jsr330ScopeMetadataResolver;
 // 1個上のdomainとかで指定すると関係ないクラスまでScanしようとしてエラーになる
 @MapperScan("com.user.mng.domain.repository")
 @ComponentScan(basePackages = "com.user.mng", scopeResolver = Jsr330ScopeMetadataResolver.class)
-public class UserMngMockApplication {
+public class UserMngMockApplication extends SpringBootServletInitializer  {
+
+	// warとしてデプロイするための設定
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserMngMockApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMngMockApplication.class, args);
